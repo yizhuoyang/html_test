453893e2ee22c5c78c3ae6f47586bf711a5114d9
yizhuoyang <1274647873@qq.com>
2024-11-15 13:41:55 +0800

    upload web

--- docs/main/_static/js/theme.js
+++ docs/main/_static/js/theme.js
@@ -714,7 +714,7 @@ function ThemeNav () {
         var self = this;
 
         // TODO this can likely be removed once the theme javascript is broken
-        // out from the RTD assets. This just ensures old projects that are
+        // out from the RTD /assets. This just ensures old projects that are
         // calling `enable()` get the sticky menu on by default. All other cals
         // to `enable` should include an argument for enabling the sticky menu.
         if (typeof(withStickyNav) == 'undefined') {
@@ -897,7 +897,7 @@ module.exports.ThemeNav = ThemeNav();
 if (typeof(window) != 'undefined') {
     window.SphinxRtdTheme = {
         Navigation: module.exports.ThemeNav,
-        // TODO remove this once static assets are split up between the theme
+        // TODO remove this once static /assets are split up between the theme
         // and Read the Docs. For now, this patches 0.3.0 to be backwards
         // compatible with a pre-0.3.0 layout.html
         StickyNav: module.exports.ThemeNav,
